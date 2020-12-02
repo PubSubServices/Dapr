10c7a2417eb045647f962e9c285ef251d8b08651
Ravind Budhiraja <Ravind.Budhiraja@publix.com>
2020-12-02 13:23:33 -0500

    Initial commit

---
+++ Dapr.PoC/Dapr.PoC/Controllers/MessageController.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Dapr.PoC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Dapr.PoC.Controllers
+{
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        private readonly ILogger<MessageController> _logger;
+        public MessageController(ILogger<MessageController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [Route("api/message")]
+        public async Task<IActionResult> ReceiveMessage([FromBody] Message message)
+        {
+            _logger.LogInformation($"Message with id {message.Id} received!");
+
+            //Validate message received
+            using (var httpClient = new HttpClient())
+            {
+                var result = await httpClient.PostAsync(
+                   "http://localhost:3500/v1.0/publish/pubsub/messagetopic",
+                   new StringContent(JsonConvert.SerializeObject(message), Encoding.UTF8, "application/json")
+                   );
+
+                _logger.LogInformation($"Message with id {message.Id} published with status {result.StatusCode}!");
+            }
+
+            return Ok();
+        }
+
+        [Topic("pubsub", "messagetopic")]
+        [HttpPost]
+        [Route("messagetopic")]
+        public async Task<IActionResult> ProcessOrder([FromBody] DaprEvent<Message> message)
+        {
+            //Process message placeholder
+            _logger.LogInformation($"Message with id {message.data.Id.ToString()} processed!");
+            return Ok();
+        }
+    }
+}
---
+++ Dapr.PoC/Dapr.PoC/Models/Message.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Dapr.PoC.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string Content { get; set; }
+        public string Subject { get; set; }
+    }
+}
