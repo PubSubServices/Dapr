bb94e2e85aee14cea02776897520b2735010829d
gregory mertens <gregory.mertens@perficient.com>
2020-12-02 08:46:55 -0600

    PS titles

--- MicroServicesDemo/ServicesStartUpA.ps1
+++ MicroServicesDemo/ServicesStartUpA.ps1
@@ -1,3 +1,4 @@
+Write-Host 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
 cd C:\Users\gregory.mertens\source\repos\Publix\MicroServicesDemo\src\MicroServiceA
 dapr run --app-id pubsub --app-port 5010  dotnet run
 
